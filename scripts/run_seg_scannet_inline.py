16e841fba7da9ea19b3f792cca01de68bfe2fbed
Leunar Kalludra <leunarkalludra@gmail.com>
2025-02-11 20:55:08 +0100

    Added tensor to pcd, Added inline segmentation script to allow debugging, Added tensor to enable imports and debugging

---
+++ scripts/run_seg_scannet_inline.py
@@ -0,0 +1,117 @@
+# --------------------------------------------------------
+# OctFormer: Octree-based Transformers for 3D Point Clouds
+# Copyright (c) 2023 Peng-Shuai Wang <wangps@hotmail.com>
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Peng-Shuai Wang
+# --------------------------------------------------------
+
+import os
+import argparse
+import shlex
+import sys
+from pathlib import Path
+from segmentation import SegSolver
+from tools.seg_scannet import *
+
+import torch 
+import numpy as np
+torch.set_printoptions(sci_mode=False)
+np.set_printoptions(suppress=True)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--run', type=str, required=False, default='validate')
+parser.add_argument('--alias', type=str, required=False, default='scannet')
+parser.add_argument('--gpu', type=str, required=False, default='0')
+parser.add_argument('--port', type=str, required=False, default='10001')
+parser.add_argument('--ckpt', type=str, required=False, default='\'\'')
+args = parser.parse_args()
+
+
+def execute_command(cmds):
+  cmds = " ".join(cmds)
+  argv = shlex.split(cmds)
+  argv = argv[1:]
+  sys.argv = argv
+  print('Execute Args: \n' + str(argv) + '\n')
+  script = Path(argv[0]).stem
+  if script == "segmentation":
+    SegSolver.main()
+  elif script == "seg_scannet":
+    eval('%s()' % args.run)
+
+
+def train():
+  cmds = [
+      'python segmentation.py',
+      '--config configs/seg_scannet.yaml',
+      'SOLVER.gpu  {},'.format(args.gpu),
+      'SOLVER.alias  {}'.format(args.alias),
+      'SOLVER.dist_url tcp://localhost:{}'.format(args.port),]
+  execute_command(cmds)
+
+def test():
+  # get the predicted probabilities for each point
+  ckpt = ('logs/scannet/octformer_{}/best_model.pth'.format(args.alias)
+          if args.ckpt == '\'\'' else args.ckpt)   # use args.ckpt if provided
+  cmds = [
+      'python segmentation.py',
+      '--config configs/seg_scannet.yaml',
+      'LOSS.mask -255',       # to keep all points
+      'SOLVER.gpu  {},'.format(args.gpu),
+      'SOLVER.run evaluate',
+      'SOLVER.eval_epoch 72',  # voting with 72 predictions
+      'SOLVER.alias test_{}'.format(args.alias),
+      'SOLVER.ckpt {}'.format(ckpt),
+      'DATA.test.batch_size 1',
+      'DATA.test.location', 'data/scannet.npz/test',
+      'DATA.test.filelist', 'data/scannet.npz/scannetv2_test_npz.txt',
+      'DATA.test.distort True', ]
+  execute_command(cmds)
+
+  # map the probabilities to labels
+  cmds = [
+      'python tools/seg_scannet.py',
+      '--run generate_output_seg',
+      '--path_pred logs/scannet/octformer_test_{}'.format(args.alias),
+      '--path_out logs/scannet/octformer_test_seg_{}'.format(args.alias),
+      '--filelist  data/scannet.npz/scannetv2_test.txt', ]
+  execute_command(cmds)
+
+
+def validate():
+  # get the predicted probabilities for each point
+  ckpt = ('logs/scannet/octformer_{}/best_model.pth'.format(args.alias)
+          if args.ckpt == '\'\'' else args.ckpt)   # use args.ckpt if provided
+  cmds = [
+      'python segmentation.py',
+      '--config configs/seg_scannet.yaml',
+      'LOSS.mask -255',       # to keep all points
+      'SOLVER.gpu  {},'.format(args.gpu),
+      'SOLVER.run evaluate',
+      'SOLVER.eval_epoch 1',  # voting with 120 predictions
+      'SOLVER.alias val_{}'.format(args.alias),
+      'SOLVER.ckpt {}'.format(ckpt),
+      'DATA.test.batch_size 1',
+      'DATA.test.distort True',]
+  execute_command(cmds)
+
+  # map the probabilities to labels
+  cmds = [
+      'python tools/seg_scannet.py',
+      '--run generate_output_seg',
+      '--path_pred logs/scannet/octformer_val_{}'.format(args.alias),
+      '--path_out  logs/scannet/octformer_val_seg_{}'.format(args.alias),
+      '--filelist  data/scannet.npz/scannetv2_val_npz.txt', ]
+  execute_command(cmds)
+
+  # calculate the mIoU
+  cmds = [
+      'python tools/seg_scannet.py',
+      '--run calc_iou',
+      '--path_in data/scannet.npz/train',
+      '--path_pred logs/scannet/octformer_val_seg_{}'.format(args.alias), ]
+  execute_command(cmds)
+
+
+if __name__ == '__main__':
+  eval('%s()' % args.run)
